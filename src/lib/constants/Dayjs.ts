2ec140a9eb50768c618c7ac8fc4509c571166a78
4lch4 <git@4lch4.email>
2023-01-16 13:37:13 -0600

    feat: added CONTACT_DATE_REGEX

--- src/lib/constants/Dayjs.ts
+++ src/lib/constants/Dayjs.ts
@@ -24,3 +24,6 @@ export function getLatestContactDate(request: any) {
     request.query.lastContact || request.body.lastContact || Dayjs().format(LATEST_CONTACT_FORMAT)
   )
 }
+
+export const CONTACT_DATE_REGEX =
+  /^\d{4}-\d{2}-\d{2}(T\d{2}:\d{2}(:\d{2})?(\.\d+)?(([+-]\d{2}:\d{2})|Z)?)?$/
